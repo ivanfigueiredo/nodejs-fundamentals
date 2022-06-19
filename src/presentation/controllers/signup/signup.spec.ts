6bca9ab17c39c073ebaf452bda718c238de3da21
ivanfigueiredo <ivanfigueiredo.wm@gmail.com>
2022-06-19 12:17:40 -0300

    test: ensure SingUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -25,8 +25,8 @@ const makeAddAccount = (): AddAccount => {
         name: 'valid_name',
         email: 'valid_email@mail.com',
         password: 'valid_password',
-        driver_license: 'valid_driver_license',
-        admin: true
+        username: 'valid_username',
+        driver_license: 'valid_driver_license'
       }
       return fakeAccount
     }
@@ -45,72 +45,101 @@ const makeSut = (): SutTypes => {
   }
 }
 describe('SignUp Controller', () => {
-  test('should return 400 if no name is provided', () => {
+  test('should return 400 if no name is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
         password: 'any_password',
         username: 'any_username',
-        driver_license: 'any_driver_license',
-        admin: false
+        driver_license: 'any_driver_license'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
-  test('should return 400 if no email is provided', () => {
+  test('should return 400 if no email is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
         password: 'any_password',
         username: 'any_username',
-        driver_license: 'any_driver_license',
-        admin: false
+        driver_license: 'any_driver_license'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('should return 400 if no password is provided', () => {
+  test('should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
         username: 'any_username',
-        driver_license: 'any_driver_license',
-        admin: false
+        driver_license: 'any_driver_license'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  test('should return 400 if no driver_license is provided', () => {
+  test('should return 400 if no driver_license is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
-        username: 'any_username',
-        admin: false
+        username: 'any_username'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('driver_license'))
   })
 
-  test('should return 400 if no admin is provided', () => {
+  test('should return 400 if no username is provided', async () => {
     const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        driver_license: 'any_driver_license'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('username'))
+  })
+
+  test('should return 400 if an invalid email is provided', async () => {
+    const { sut, emailValidator } = makeSut()
+    jest.spyOn(emailValidator, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        username: 'any_username',
+        driver_license: 'any_driver_license'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('should call EmailValidator with correct email', async () => {
+    const { sut, emailValidator } = makeSut()
+    const spyIsvalid = jest.spyOn(emailValidator, 'isValid')
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -120,47 +149,11 @@ describe('SignUp Controller', () => {
         driver_license: 'any_driver_license'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('admin'))
-  })
-
-  test('should return 400 if an invalid email is provided', () => {
-    const { sut, emailValidator } = makeSut()
-    jest.spyOn(emailValidator, 'isValid').mockReturnValueOnce(false)
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'invalid_email@mail.com',
-        password: 'any_password',
-        username: 'any_username',
-        driver_license: 'any_driver_license',
-        admin: true
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
-  })
-
-  test('should call EmailValidator with correct email', () => {
-    const { sut, emailValidator } = makeSut()
-    const spyIsvalid = jest.spyOn(emailValidator, 'isValid')
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        username: 'any_username',
-        driver_license: 'any_driver_license',
-        admin: true
-      }
-    }
-    sut.handle(httpRequest)
+    await sut.handle(httpRequest)
     expect(spyIsvalid).toHaveBeenCalledWith(httpRequest.body.email)
   })
 
-  test('should return 500 if EmailValidator throws', () => {
+  test('should return 500 if EmailValidator throws', async () => {
     const { sut, emailValidator } = makeSut()
     jest.spyOn(emailValidator, 'isValid').mockImplementationOnce(() => { throw new Error() })
     const httpRequest = {
@@ -169,16 +162,15 @@ describe('SignUp Controller', () => {
         email: 'any_email@mail.com',
         password: 'any_password',
         username: 'any_username',
-        driver_license: 'any_driver_license',
-        admin: true
+        driver_license: 'any_driver_license'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
-  test('should call AddAccount with correct values', () => {
+  test('should call AddAccount with correct values', async () => {
     const { sut, addAccountStub } = makeSut()
     const spyAdd = jest.spyOn(addAccountStub, 'add')
     const httpRequest = {
@@ -191,11 +183,11 @@ describe('SignUp Controller', () => {
         admin: true
       }
     }
-    sut.handle(httpRequest)
+    await sut.handle(httpRequest)
     expect(spyAdd).toHaveBeenCalledWith(httpRequest.body)
   })
 
-  test('should return 500 if AddAccount throws', () => {
+  test('should return 500 if AddAccount throws', async () => {
     const { sut, addAccountStub } = makeSut()
     jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => { throw new Error() })
     const httpRequest = {
@@ -204,12 +196,34 @@ describe('SignUp Controller', () => {
         email: 'any_email@mail.com',
         password: 'any_password',
         username: 'any_username',
-        driver_license: 'any_driver_license',
-        admin: true
+        driver_license: 'any_driver_license'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password',
+        username: 'valid_username',
+        driver_license: 'valid_driver_license'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+      username: 'valid_username',
+      driver_license: 'valid_driver_license'
+    })
   })
 })
