6c1e8ba090aec4d9042e6edcbf2cf02152537fc6
ivanfigueiredo <ivanfigueiredo.wm@gmail.com>
2022-06-19 14:24:39 -0300

    test: should return if validator returns true

--- src/infra/utils/validators/email-validator-adapter.spec.ts
+++ src/infra/utils/validators/email-validator-adapter.spec.ts
@@ -24,4 +24,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
+
+  test('should return if validator returns true', () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
